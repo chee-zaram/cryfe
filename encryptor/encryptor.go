8637deb59b65c227dfe28f85ffa72bc7cf508f6c
Emmanuel Chee-zaram Okeke <cheezaram.okeke@gmail.com>
2023-09-26 17:27:03 +0100

    encryptor: define encryption function with helpers

---
+++ encryptor/encryptor.go
@@ -0,0 +1,58 @@
+package encryptor
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"fmt"
+	"io"
+	"os"
+)
+
+// readInputFile calls os.ReadFile to read a file, and returns the plain text
+// and any error that may have occured.
+func readInputFile(inputFile string) ([]byte, error) {
+	return os.ReadFile(inputFile)
+}
+
+// createAES_CipherBlock takes in a key and returns a block cipher and any error
+// which may have occured.
+func createAES_CipherBlock(key []byte) (cipher.Block, error) {
+	if key == nil {
+		return nil, fmt.Errorf("key cannot be nil")
+	}
+
+	return aes.NewCipher(key)
+}
+
+// readRandBytesToBuf takes a slice of bytes and reads randome bytes into it
+// from rand.Reader.
+func readRandBytesToBuf(buf []byte) (int, error) {
+	return io.ReadFull(rand.Reader, buf)
+}
+
+// EncryptFile encrypts a file using AES algorithm if it is available.
+// It returns an error if any occurs.
+func EncryptFile(inputFile, outputFile string, key []byte) error {
+	plainText, err := readInputFile(inputFile)
+	if err != nil {
+		return err
+	}
+
+	block, err := createAES_CipherBlock(key)
+	if err != nil {
+		return err
+	}
+
+	cipherText := make([]byte, aes.BlockSize+len(plainText))
+	buf := cipherText[:aes.BlockSize]
+
+	if _, err := readRandBytesToBuf(buf); err != nil {
+		return err
+	}
+
+	mode := cipher.NewCBCEncrypter(block, buf)
+	mode.CryptBlocks(cipherText[aes.BlockSize:], plainText)
+
+	return os.WriteFile(outputFile, cipherText, 0644)
+}
