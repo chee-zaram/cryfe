2a354732ec20c4d9cc4089821b1eb45ed4c1e6bf
Emmanuel Chee-zaram Okeke <cheezaram.okeke@gmail.com>
2023-09-27 11:29:42 +0100

    encryptor: add test for `encryptor` package

--- encryptor/encryptor_test.go
+++ encryptor/encryptor_test.go
@@ -1,19 +1,18 @@
 package encryptor
 
 import (
+	"crypto/aes"
 	"os"
 	"testing"
 )
 
 func Test_readInputFile(t *testing.T) {
-	// Create a temporary file for testing
 	tmpfile, err := os.CreateTemp("", "testfile")
 	if err != nil {
 		t.Fatal(err)
 	}
 	defer os.Remove(tmpfile.Name())
 
-	// Write some data to the temporary file
 	data := []byte("test data")
 	if _, err := tmpfile.Write(data); err != nil {
 		t.Fatal(err)
@@ -22,21 +21,17 @@ func Test_readInputFile(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	// Call the function under test
 	result, err := readInputFile(tmpfile.Name())
-	// Check if there was an error
 	if err != nil {
 		t.Errorf("readInputFile returned an error: %v", err)
 	}
 
-	// Check if the returned data matches the expected data
 	if string(result) != string(data) {
 		t.Errorf("readInputFile returned incorrect data, got: %s, want: %s", string(result), string(data))
 	}
 }
 
 func Test_readInputFile_NonExistentFile(t *testing.T) {
-	// Call the function under test with a non-existent file
 	_, err := readInputFile("nonexistent.txt")
 	if err == nil {
 		t.Error("readInputFile did not return an error for a non-existent file")
@@ -44,8 +39,81 @@ func Test_readInputFile_NonExistentFile(t *testing.T) {
 }
 
 func Test_createAES_CipherBlock(t *testing.T) {
+	key := make([]byte, 16) // AES-128 key
+
+	block, err := createAES_CipherBlock(key)
+	if err != nil {
+		t.Fatalf("Error creating AES cipher block: %v", err)
+	}
+	if block.BlockSize() != aes.BlockSize {
+		t.Errorf("Expected block size %d, but got %d", aes.BlockSize, block.BlockSize())
+	}
+}
+
+func Test_createAES_CipherBlock_NilKey(t *testing.T) {
 	_, err := createAES_CipherBlock(nil)
 	if err == nil {
-		t.Errorf("createAES_CipherBlock failed to return an error")
+		t.Errorf("Expected an error for a nil key, but got nil")
 	}
+}
+
+func Test_readRandBytesToBuf(t *testing.T) {
+	buf := make([]byte, 16)
+
+	n, err := readRandBytesToBuf(buf)
+	if err != nil {
+		t.Fatalf("Error reading random bytes: %v", err)
+	}
+	if n != len(buf) {
+		t.Errorf("Expected to read %d bytes, but got %d", len(buf), n)
+	}
+}
+
+func Test_readRandBytesToBuf_NilBuffer(t *testing.T) {
+	n, _ := readRandBytesToBuf(nil)
+	if n != 0 {
+		t.Errorf("Expected number of bytes read to be 0, got %d", n)
+	}
+}
+
+/* func TestEncryptFileAndDecryptFile(t *testing.T) {
+	key := make([]byte, 16) // AES-128 key
+
+	tmpFile := createTempFile(t)
+	defer os.Remove(tmpFile.Name())
+
+	plainText := []byte("Testcontent")
+	if err := os.WriteFile(tmpFile.Name(), plainText, 0644); err != nil {
+		t.Fatal(err)
+	}
+
+	encryptedFile := tmpFile.Name() + ".enc"
+	err := EncryptFile(tmpFile.Name(), encryptedFile, key)
+	if err != nil {
+		t.Fatalf("Error encrypting file: %v", err)
+	}
+
+	decryptedFile := tmpFile.Name() + ".dec"
+	err = DecryptAES_File(encryptedFile, decryptedFile, key)
+	if err != nil {
+		t.Fatalf("Error decrypting file: %v", err)
+	}
+
+	decryptedContent, err := os.ReadFile(decryptedFile)
+	if err != nil {
+		t.Fatalf("Error reading decrypted file: %v", err)
+	}
+
+	if string(plainText) != string(decryptedContent) {
+		t.Errorf("Decrypted content does not match original content")
+	}
+} */
+
+// createTempFile creates a temporary test file and returns a file handle.
+func createTempFile(t *testing.T) *os.File {
+	tmpFile, err := os.CreateTemp("", "testfile")
+	if err != nil {
+		t.Fatalf("Error creating temporary test file: %v", err)
+	}
+	return tmpFile
 }
